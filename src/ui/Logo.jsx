db94bb4f3ae870989145a01e54aa9e2e08e54798
atharvamulam4 <2021.atharva.mulam@ves.ac.in>
2023-09-13 16:00:13 +0530

    logo img

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -3,7 +3,7 @@ import "../index.css"
 
 function Logo(){
   return (
-    <NavLink to="/"  src="../../public/Logo.jpeg" alt="Logo" className="logo"></NavLink>
+    <NavLink to="/"><img src="../../public/Logo.jpeg" alt="Logo" className="logo" /></NavLink>
   )
 }
 export default Logo;
