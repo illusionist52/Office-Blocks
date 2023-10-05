9964b1bd5b5a0954bd736b320c15c60d740e6273
usmaan <2021.mohdusmaan.mogal@ves.ac.in>
2023-10-05 20:17:07 +0530

    logout btn

--- src/ui/Nav.jsx
+++ src/ui/Nav.jsx
@@ -4,9 +4,17 @@ import "../pages/Homepage.css"
 import Logo from "./Logo";
 import Button from "./Button";
 import LinkButton from "./LinkButton";
+import { BiSolidUser } from "react-icons/bi";
+import { useState } from "react";
 function Nav(){
-  const userData = JSON.parse(localStorage.getItem("token"));
+  let usd=JSON.parse(localStorage.getItem("token"))
+  const [userData,setUserData] = useState(usd) ;
   
+  function Logout(){
+    localStorage.removeItem("token");
+    setUserData(usd);
+  }
+
   return <nav className="nav">
     <Logo/>
     <ul>
@@ -26,7 +34,8 @@ function Nav(){
     
     {!userData && <LinkButton to={"/signup"} style={"btn primary-btn"}>Sign up</LinkButton>}
     {!userData && <LinkButton to={"/login"} style={"btn secondary-btn"}>Log in</LinkButton>}
-    {userData && <p>welcome {userData.username}</p>}
+    {userData && <span><BiSolidUser color="" size={25}/> {userData.username}</span>}
+    {userData && <Button style={"btn secondary-btn"} onClick={Logout} >Log out</Button>}
   </nav>
 }
 
