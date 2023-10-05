8c277a386a0991f4c5acd64a3405bbebebf1b438
usmaan <2021.mohdusmaan.mogal@ves.ac.in>
2023-10-05 11:25:49 +0530

    Property Data connected.

--- src/pages/Properties.jsx
+++ src/pages/Properties.jsx
@@ -3,21 +3,21 @@ import SearchBar from "../ui/Searchbar";
 import "./Properties.css";
 import { useEffect } from "react";
 // import useProperties from "../../hooks/useProperties";
-// import { PuffLoader } from "react-spinners";
+import { PuffLoader } from "react-spinners";
 import PropertyCard from "../ui/PropertyCard";
 import { useQueries, useQuery } from "@tanstack/react-query";
 import getProperties from "../services/apiProperties";
 const Properties = () => {
   // const { data, isError, isLoading } = useProperties();
   const [filter, setFilter] = useState("");
-  // const {data, error}= useQuery({
-  //   queryKey: ["properties"],
-  //   queryFn: getProperties
-  // })
+  const {data, error, isLoading}= useQuery({
+    queryKey: ["properties"],
+    queryFn: getProperties
+  })
   // useEffect(function(){
   //   getProperties().then((data)=>console.log(data))
   // })
-  
+console.log(data)
   // if (isError) {
   //   return (
   //     <div className="wrapper">
@@ -26,34 +26,31 @@ const Properties = () => {
   //   );
   // }
 
-  // if (isLoading) {
-  //   return (
-  //     <div className="wrapper flexCenter" style={{ height: "60vh" }}>
-  //       <PuffLoader
-  //         height="80"
-  //         width="80"
-  //         radius={1}
-  //         color="#4066ff"
-  //         aria-label="puff-loading"
-  //       />
-  //     </div>
-  //   );
-  // }
+  if (isLoading) {
+    return (
+      <div className="wrapper flexCenter" style={{ height: "60vh" }}>
+        <PuffLoader
+          height="80"
+          width="80"
+          radius={1}
+          color="#4066ff"
+          aria-label="puff-loading"
+        />
+      </div>
+    );
+  }
   return (
     <div className="wrapper">
       <div className="flexColCenter paddings innerWidth properties-container">
         <SearchBar filter={filter} setFilter={setFilter} />
 
-        <div className="paddings flexCenter properties">
+        <div className="paddings flexCenter properties grid--2cols">
           {
-            // data.map((card, i)=> (<PropertyCard card={card} key={i}/>))
+            data.map((card, i)=> (<PropertyCard card={card} key={i}/>))
 
            
           }
-          <PropertyCard/>
-          <PropertyCard/>
-          <PropertyCard/>
-          <PropertyCard/>
+         
         </div>
       </div>
     </div>
--- src/ui/PropertyCard.jsx
+++ src/ui/PropertyCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './PropertyCard.css'
 import {truncate} from 'lodash'
 import { useNavigate } from "react-router-dom";
-const PropertyCard = () => {
+const PropertyCard = ({card}) => {
 
   const navigate = useNavigate();
   return (
@@ -12,10 +12,10 @@ const PropertyCard = () => {
       <img src="../../public/office-1.png" alt="home" />
       <span className="secondaryText r-price">
         <span style={{ color: "orange" }}>$</span>
-        <span> 9</span>
+        <span> {card.price}</span>
       </span>
-      <span className="primaryText">00</span>
-      <span className="secondaryText">000</span>
+      <span className="primaryText">{truncate(card.title, {length: 15})}</span>
+      <span className="secondaryText">{truncate(card.description, {length: 80})}</span>
     </div>
   );
 };
